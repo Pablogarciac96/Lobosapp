cf51a8c104662aa7eb70053aa26913e6f99fdaf0
Pablogarciac96 <pcgarcia2019-2@corhuila.edu.co>
2020-04-26 21:25:20 -0500

    Commit

---
+++ app/src/main/java/com/garcua/lobosapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.garcua.lobosapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+    private final int duracion = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable(){
+            //cambio de actividad
+            public void run(){
+               // Intent intent = new Intent(MainActivity.this, login.class);
+                getIntent().setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+             //   startActivity(intent);
+                finish();
+            };
+        }, duracion);
+
+    }
+}
